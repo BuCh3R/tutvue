281bb6880fc1ee5f271c9b7f635dd098a617880e
Morten Buch <mobu26304@edu.ucl.dk>
2024-04-03 11:32:26 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import UserInput from '@/components/UserInput.vue';
+</script>
+
+<template>
+  <UserInput label="Navn"/><br><br>
+  <UserInput label="Yndlingsfarve"/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/UserInput.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+const messege = ref('');
+defineProps({
+    label: String
+});
+</script>
+
+<template>
+    <span>{{ label }}</span>
+    <input type="text" v-model="messege">
+    <p>{{ messege }}</p>
+</template>
